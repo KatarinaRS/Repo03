143aabc0ba57e0ebf20fc4fc87df5ef799b9bbe8
KatarinaRS <katarina.roser@gmail.com>
2022-03-30 21:53:50 +0200

    Substract and multiplying

--- ex10/src/index.js
+++ ex10/src/index.js
@@ -2,15 +2,15 @@ function main(numOne, numTwo) {
     // Only change code below this line
         var numOne;
         var numTwo;
-        var difference;
-        difference = numOne - numTwo;
+        var product;
+        product = numOne * numTwo;
     // Only change code above this line
-    return difference;
+    return product;
     
 }
 
-console.log(main(15, 8)); // change this line
+console.log(main(15, 3)); // change this line
 console.log(main(25, 8)); // change this line
-console.log(main(425, 120)); // change this line
+console.log(main(425, 10)); // change this line
 
 module.exports = main;
